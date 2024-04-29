9f6d182626b4afb9346119becc3741700846f37b
latercomer <latercomer@qq.com>
2024-04-29 11:20:48 +0800

    重新测试了

--- env.ps1
+++ env.ps1
@@ -1,45 +1,37 @@
 
-
-function install(){
-    
-
-}
-
-
 $tz = Get-TimeZone
 if ($tz.BaseUtcOffset.TotalHours -eq 8) {
-    $python_url = "https://registry.npmmirror.com/-/binary/python/3.12.3/python-3.12.3-amd64.exe"
-    $pkg_url = "https://github.com/rt-thread/packages"
-    $sdk_url = "https://github.com/rt-thread/sdk"
-    $git_url = "https://registry.npmmirror.com/-/binary/git-for-windows/v2.44.0.windows.1/MinGit-2.44.0-64-bit.zip"
+    $python_amd64_url = "https://registry.npmmirror.com/-/binary/python/3.12.3/python-3.12.3-amd64.exe"
+    $git_for_windows_url = "https://registry.npmmirror.com/-/binary/git-for-windows/v2.44.0.windows.1/MinGit-2.44.0-64-bit.zip"
+
+    $pkg_url = "https://github.com/rt-thread/packages.git"
+    $sdk_url = "https://github.com/rt-thread/sdk.git"
+    $env_url = "https://gitee.com/latercomer/rtt-env.git"
 } else {
-    $python_url = "https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe"
-    $pkg_url = "https://github.com/rt-thread/packages"
-    $sdk_url = "https://github.com/rt-thread/sdk"
-    $git_url = "https://registry.npmmirror.com/-/binary/git-for-windows/v2.44.0.windows.1/MinGit-2.44.0-64-bit.zip"
+    $python_amd64_url = "https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe"
+    $git_for_windows_url = "https://registry.npmmirror.com/-/binary/git-for-windows/v2.44.0.windows.1/MinGit-2.44.0-64-bit.zip"
+
+    $pkg_url = "https://github.com/rt-thread/packages.git"
+    $sdk_url = "https://github.com/rt-thread/sdk.git"
+    $env_url = "https://github.com/rt-thread/env.git"
 }
 
-# settings = {}
-if (Test-Path -path "$PSScriptRoot\settings.json") {
-    
+$setting = @{
+    pkg_index_path   = "manifests/packages";
+    sdk_index_path   = "manifests/sdk";
+    sdk_install_path = "program";
+    env              = {
+        PKGS_DIR="$PSScriptRoot/manifests";
+        PKGS_ROOT="$PSScriptRoot/manifests";
+        ENV_ROOT="$PSScriptRoot";
+    }
 }
 
+$setting | ConvertTo-Json | Out-File "$PSScriptRoot\setting.json"
 
 function Test-Command([string] $cmd) {
     ($null -ne (Get-Command $cmd -ErrorAction SilentlyContinue))
 }
-
-
-# # install rtt-pkg
-# if (-not (Test-Path -Path "$PSScriptRoot\manifests\packages\.git")) {
-#     git clone $pkg_url $PSScriptRoot\manifests\packages
-# }
-
-# # install rtt-sdk
-# if (-not (Test-Path -Path "$PSScriptRoot\manifests\toolchain\.git")) {
-#     git clone $sdk_url $PSScriptRoot\manifests\toolchain
-# }
-
 
 # install python
 if (!(Test-Command python)) {
@@ -62,14 +54,23 @@ if (-not (Test-Path -Path $VENV_ROOT)) {
     # 激活python venv
     & "$VENV_ROOT\Scripts\Activate.ps1"
     # 安装env-script
-    pip install "$PSScriptRoot\scripts"
+    pip install "git+$env_url"
 } else {
     # 激活python venv
     & "$VENV_ROOT\Scripts\Activate.ps1"
 }
 
+# clone rtt-pkg
+if (-not (Test-Path -Path "$PSScriptRoot\manifests\packages\.git")) {
+    git clone $pkg_url $PSScriptRoot\manifests\packages
+}
+
+# clone rtt-sdk
+if (-not (Test-Path -Path "$PSScriptRoot\manifests\toolchain\.git")) {
+    git clone $sdk_url $PSScriptRoot\manifests\toolchain
+}
+
 $env:ENV_ROOT = "$PSScriptRoot"
 $env:PKGS_ROOT = "$PSScriptRoot\manifests"
 $env:PKGS_DIR = "$PSScriptRoot\manifests"
-
 $env:pathext = ".ps1; $env:pathext"
